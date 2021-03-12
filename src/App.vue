ac65772f24343216df5c5ccb36ab6a2252517a02
kazuki komatsu <spitzspitzspitz14@icloud.com>
2021-03-12 15:37:26 +0900

    first-commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,27 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <input type="text" v-model= "number">
+    <button @click= "serchAddress">住所自動入力</button>
+    <p>Address：{{ address }}</p>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return{
+      number:""
+    }
+  },
+  methods: {
+    serchAddress(){
+      
+    },
+    created(){
+      axios.get(`https://apis.postcode-jp.com/api/v4/postcodes/${this.address}/HLzIChi5hK02FZ8XOpCFGYFzmP0FJRb7UDFpUHS`)
+      .then()
+    }
+  }
+};
+</script>
